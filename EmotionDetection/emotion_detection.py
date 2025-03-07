ad305a45133850566d8fe29d376ce62de9ff89af
Abdin71 <abdin.huss@gmail.com>
2025-03-07 16:42:17 +0100

    Packaged emotion detector

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,4 +6,8 @@ def emotion_detector(text_to_analyse):
     myobj = { "raw_document": { "text": text_to_analyse } }
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}  
     response = requests.post(url, json=myobj, headers=header)  
-    return response.text
+    formatted_response = json.loads(response.text)
+    emotion = formatted_response['emotionPredictions'][0]['emotion']
+    dominant_emotion = max(emotion, key=emotion.get)
+    return {'anger': emotion['anger'], 'disgust': emotion['disgust'], 'fear': emotion['fear'], 
+        'joy': emotion['joy'], 'sadness': emotion['sadness'], 'dominant_emotion': dominant_emotion}
